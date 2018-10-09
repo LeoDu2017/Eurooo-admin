b3f0faa1e749b0a2e64632a6cacd346728ca3daa
LeoDu2017 <2516849822@qq.com>
2018-10-09 15:23:13 +0800

    内页布局- 店铺管理员

---
+++ src/components/modal/admin-info-form.js
@@ -0,0 +1,130 @@
+import intl from 'react-intl-universal';
+import { Modal, Form, Input,Radio } from 'antd';
+import { connect } from 'dva';
+import { showModelHandler,hideModelHandler,okHandler } from 'Actions/common-modal';
+
+const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
+
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
+};
+
+
+
+const UserEditModal = ({dispatch,children,visible,id,add,onOk,record,form:{getFieldDecorator,validateFields,resetFields}})=> (
+  <span>
+      <span onClick={showModelHandler.bind(null,dispatch,id)}>
+        { children }
+      </span>
+      <Modal
+        title= {add ? `${intl.get('ADD')}${intl.get('ADMIN')}` : `${intl.get('EDIT')}${intl.get('ADMIN')}`}
+        visible={visible[id]}
+        onOk={okHandler.bind(null,dispatch,validateFields,onOk,id,false)}
+        onCancel={hideModelHandler.bind(null,dispatch,resetFields,id)}
+      >
+        <Form horizontal="true">
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('USERNAME')}
+            style={add ? {display:'block'} : {display:'none'}}
+          >
+            {
+              getFieldDecorator('username', {
+                initialValue:record.username,
+                rules: [{required: true, message:intl.get('INPUTUSERNAME')}],
+              })(<Input placeholder={intl.get('INPUTUSERNAME')} />)
+            }
+          </FormItem>
+          {
+            add && <FormItem
+              {...formItemLayout}
+              label={intl.get('PASSWORD')}
+              style={add ? {display:'block'} : {display:'none'}}
+            >
+              {
+                getFieldDecorator('password', {
+                  initialValue:record.password,
+                  rules: [{required: true, message:intl.get('INPUTUPASSWORD')}],
+                })(<Input placeholder={intl.get('INPUTUPASSWORD')} />)
+              }
+            </FormItem>
+          }
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('NAME')}
+          >
+            {
+              getFieldDecorator('name', {
+                initialValue:record.name,
+                rules: [{required: true, message:intl.get('INPUTNAME')}],
+              })(<Input placeholder={intl.get('INPUTNAME')} />)
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('TITLE')}
+          >
+            {
+              getFieldDecorator('title', {
+                initialValue:record.title,
+              })(<Input placeholder={intl.get('INPUTADMINTITLE')} />)
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('CONTACT')}
+          >
+            {
+              getFieldDecorator('contactNumber', {
+                initialValue:record.contactNumber,
+                rules: [{required: true, message:intl.get('INPUTCONTACT')}],
+              })(<Input placeholder={intl.get('INPUTCONTACT')}/>)
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('ACCOUNTSTATUS')}
+          >
+            {
+              getFieldDecorator('userMode', {
+                initialValue:record.userMode,
+              })(
+                <RadioGroup>
+                  <Radio value={1}>{intl.get('INUSE')}</Radio>
+                  <Radio value={0}>{intl.get('CLOSE')}</Radio>
+                </RadioGroup>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={intl.get('AUTHORIZATION')}
+          >
+            {
+              getFieldDecorator('permissions', {
+                initialValue:record.permissions,
+                rules: [{required: true, message:intl.get('SELECTAUTHORIZATION')}],
+              })(
+                <RadioGroup>
+                  <Radio value={1}>{intl.get('INAUTH')}</Radio>
+                  <Radio value={0}>{intl.get('OUTAUTH')}</Radio>
+                </RadioGroup>
+              )
+            }
+          </FormItem>
+        </Form>
+      </Modal>
+    </span>
+);
+
+function mapStateToProps(state){
+  const {visible} = state.commonModal;
+  return{
+    visible
+  }
+}
+
+export default connect(mapStateToProps)(Form.create()(UserEditModal));
+
---
+++ src/pages/spots/shop/admin.js
@@ -0,0 +1,97 @@
+import intl from 'react-intl-universal';
+import { connect } from 'dva';
+import { Table,Divider,Tag,Col,Button,Icon } from 'antd';
+import { deleteAdmin,createAdmin,editHandler,resetPassword } from 'Actions/shop';
+
+import UserModal from 'Components/modal/admin-info-form';
+
+const adminTable = ({dispatch,shopAdmins}) => {
+  const columns = [{
+    title: intl.get('AVATAR'),
+    dataIndex: 'avatar',
+    key: 'avatar',
+    render: (data,record) =><img width={26} src={data} alt={record.username}/>,
+  },{
+    title: intl.get('USERNAME'),
+    dataIndex: 'username',
+    key: 'username',
+    render: text => <a href="javascript:">{text}</a>,
+  }, {
+    title: intl.get('NAME'),
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: intl.get('TITLE'),
+    key: 'title',
+    dataIndex: 'title',
+    render: titles => (
+      <span>
+        {titles.split(',').map(title => <Tag color="blue" key={title}>{title}</Tag>)}
+      </span>
+    ),
+  },{
+    title: intl.get('CONTACT'),
+    dataIndex: 'contactNumber',
+    key: 'contactNumber',
+    render: number => number.replace(/(^\d{3}|\d{4}\B)/g,"$1-")
+  },{
+    title: intl.get('ACCOUNTSTATUS'),
+    dataIndex: 'userMode',
+    key: 'userMode',
+    render: model => model ? intl.get('INUSE') : intl.get('CLOSE')
+  },{
+    title: intl.get('AUTHORIZATION'),
+    dataIndex: 'permissions',
+    key: 'permissions',
+    render: auth => auth ? intl.get('INAUTH') : intl.get('OUTAUTH')
+  },{
+    title: intl.get('ACTION'),
+    key: 'action',
+    render: (text, record) => (
+      <span>
+      <a href="javascript:" onClick={resetPassword.bind(null,dispatch,record.id)}>{intl.get('RESETPASSWORD')}</a>
+      <Divider type="vertical" />
+      <a href="javascript:" onClick={deleteAdmin.bind(null,dispatch,record.id)}>{intl.get('DELETE')}</a>
+      <Divider type="vertical" />
+      <UserModal record={record} add={false} id={record.id} onOk={editHandler.bind(null,dispatch,record.id)}>
+        <a href="javascript:">
+          {intl.get('EDIT')}
+        </a>
+      </UserModal>
+    </span>
+    ),
+  }];
+  return (
+    <Col className='g-t-wrap'>
+      <Col className='g-t-main'>
+        <header className='g-t-header'>
+          <span className='g-t-title'>
+            {intl.get('SHOPADMIN')}
+          </span>
+          <UserModal record={{}} id={-1} add={true} onOk={createAdmin.bind(null,dispatch)}>
+            <Button type='primary' size="small" htmlType="button">
+              <Icon type="user-add" />
+              {intl.get('ADD')}
+            </Button>
+          </UserModal>
+        </header>
+        <Col className="g-t-form-wrap">
+          <Table
+            columns={columns}
+            dataSource={shopAdmins}
+            pagination={{ pageSize: 7 }}/>
+        </Col>
+      </Col>
+    </Col>
+  )
+};
+
+function mapStateToProps(state){
+  const {shopAdmins} = state.admin;
+
+  return{
+    shopAdmins
+  }
+}
+
+export default connect(mapStateToProps)(adminTable);
