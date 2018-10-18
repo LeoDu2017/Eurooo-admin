bced423a63c99e8ac9501af126db99fba6adc58d
LeoDu2017 <2516849822@qq.com>
2018-10-18 15:06:21 +0800

    内页布局- 我的品牌列表 - 产品列表

--- src/components/modal/edit-product-info.js
+++ src/components/modal/edit-product-info.js
@@ -1,5 +1,5 @@
 import { connect } from 'dva';
-import { Modal,Tabs,Form,Input,Select,Button,Radio,Card } from 'antd';
+import { Modal,Tabs,Form,Input,Select,Button,Radio,Card,Checkbox } from 'antd';
 import { show,hide,ok } from 'Actions/common-modal';
 import PriceInput from 'Components/units/Price-input';
 import ClassSelete from 'Components/units/Class-selecte';
@@ -8,12 +8,15 @@ const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
 const Option = Select.Option;
 const RadioGroup = Radio.Group;
+const CheckboxGroup = Checkbox.Group;
+const { TextArea } = Input;
 
 const formItemLayout = {
   labelCol: { span: 3 },
   wrapperCol: { span: 21 },
 };
 const EditProductInfoFrom = ({
+        productSpaces,productStyles,
         title,visible,dispatch,classID,
         ProductClassifications,id,myBrands,children,
         form:{getFieldDecorator,validateFieldsAndScroll}}) => (
@@ -27,7 +30,7 @@ const EditProductInfoFrom = ({
       onCancel={hide.bind(null,dispatch,null,`edit-${id}`)}>
       <Form>
         <Tabs tabPosition="right">
-          <TabPane tab="基本信息" key="1">
+          <TabPane tab="基本信息" key="1" style={{height:520,overflow:'hidden',overflowY:'auto'}}>
             <FormItem {...formItemLayout} label="产品名称">
               {getFieldDecorator('name',{
                 rules: [{ required: true, message: '请输入产品名称' }],
@@ -56,8 +59,7 @@ const EditProductInfoFrom = ({
             <FormItem {...formItemLayout} label="品牌">
               {getFieldDecorator('brand', {
                 initialValue: 0,
-              })(<div style={{height:'150px',overflow:'hidden',overflowY:'auto'}}>
-                  <RadioGroup >
+              })(<RadioGroup >
                     {
                       myBrands.map( brand => <Card
                         key={brand.id}
@@ -67,8 +69,7 @@ const EditProductInfoFrom = ({
                         cover={<img alt={brand.name} style={{'padding':'10px'}} src={`${brand.logo}@110h_216w_1e_1c`} />}
                         title={brand.name}/> )
                     }
-                  </RadioGroup>
-              </div>)}
+                  </RadioGroup>)}
             </FormItem>
 
             <FormItem {...formItemLayout} label="分类">
@@ -78,6 +79,43 @@ const EditProductInfoFrom = ({
                               ProductClassifications={ProductClassifications}/>)}
             </FormItem>
 
+            <FormItem {...formItemLayout} label="空间">
+              {getFieldDecorator('space', {
+                rules: [{ required: true, message: '请输入品牌名称' }]
+              })(<CheckboxGroup>
+                {
+                  productSpaces.map(space => <Checkbox value={space.id} key={space.id}>{space.name}</Checkbox>)
+                }
+              </CheckboxGroup>)}
+            </FormItem>
+
+            <FormItem {...formItemLayout} label="风格">
+              {getFieldDecorator('style', {
+                rules: [{ required: true, message: '请输入品牌名称' }]
+              })(<Select style={{ width:250}}>
+                {
+                  productStyles.map(style => <Option value={style.id} key={style.id}>{style.name}</Option>)
+                }
+              </Select>)}
+            </FormItem>
+
+            <FormItem {...formItemLayout} label="产品描述">
+              {getFieldDecorator('description', { })(
+                <TextArea placeholder="Autosize height with minimum and maximum number of lines"
+                          autosize={{ minRows: 2, maxRows: 6 }} />
+              )}
+            </FormItem>
+
+            <FormItem {...formItemLayout} label="产品状态">
+              {getFieldDecorator('status', {
+                initialValue: 0,
+                // rules: [{ required: true, message: '请输入产品价格' }],
+                // rules: [{ validator: this.checkPrice }],
+              })(<RadioGroup>
+                <Radio value={0}>下架</Radio>
+                <Radio value={1}>上架</Radio>
+              </RadioGroup>)}
+            </FormItem>
 
           </TabPane>
           <TabPane tab="产品属性" key="2">
@@ -90,25 +128,11 @@ const EditProductInfoFrom = ({
             4
           </TabPane>
         </Tabs>
-        <Button onClick={tijiao.bind(null,validateFieldsAndScroll)}>
-          提交
-        </Button>
       </Form>
     </Modal>
   </span>
 );
-function tijiao(validateFieldsAndScroll){
-  validateFieldsAndScroll({ force: true },(err, values) => {
-    console.log(values);
-    // if (!err) {
-    //   dispatch({
-    //     type:'commonModal/setVisible',
-    //     payload:{[id]:false}
-    //   });
-    //   callBack(values);
-    // }
-  })
-}
+
 function mapStateToProps(state){
   const { visible } = state.commonModal;
   return { visible }
